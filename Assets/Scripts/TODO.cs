63d4ffde9d6c558634fae3e2c90419b6628cd5b2
Ben Stone <spector.8500@gmail.com>
2021-04-01 08:27:36 -0400

    SnapAim, SlerpAim and LerpAim finished

--- Assets/Scripts/TODO.cs
+++ Assets/Scripts/TODO.cs
@@ -3,7 +3,14 @@ public class TODO
 }
 /*
 todo Camera Zoom
-todo Rotate <- Left by degrees/second
+todo projectile
+todo instantiate projectile via input at muzzle
 
 
+
+Target: https://www.youtube.com/watch?v=AJ7lLzMgJ98&list=WL&index=7
+Tutorials:
+https://www.youtube.com/watch?v=ivoQVddFwdg&list=WL&index=6
+https://www.youtube.com/watch?v=ddnncJik4Ks&list=WL&index=7
+https://www.youtube.com/watch?v=48ENK8La7dc&list=WL&index=10
 */
--- Assets/Scripts/Tank/SlerpAim.cs
+++ Assets/Scripts/Tank/SlerpAim.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.InputSystem;
 
-public class SnapAim : MonoBehaviour
+// todo inherit from snapaim
+public class SlerpAim : MonoBehaviour
 {
     // todo move this to a utility class, game manager thing?
     [SerializeField] 
-    private Camera gameCamera;
+    private Camera _gameCamera;
+
+    [SerializeField] 
+    private float _rotationSpeed = 0.5f;
     
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Assert(gameCamera != null);
+        Debug.Assert(_gameCamera != null);
     }
 
     // FixedUpdate since the barrel will have a collider and might hit a wall
@@ -24,7 +28,7 @@ public class SnapAim : MonoBehaviour
 
         if (mouse != null)
         {
-            var worldMousePos = gameCamera.ScreenToWorldPoint(mouse.position.ReadValue());
+            var worldMousePos = _gameCamera.ScreenToWorldPoint(mouse.position.ReadValue());
             //Debug.DrawLine(worldMousePos, worldMousePos + new Vector3(1, 0, 0), Color.green, 1f); // not showing up in Game View
             //Debug.DrawLine(worldMousePos, worldMousePos + new Vector3(0, 1, 0), Color.green);
 
@@ -36,11 +40,13 @@ public class SnapAim : MonoBehaviour
 
             float angle = Mathf.Atan2(directionToFace.y,directionToFace.x) * Mathf.Rad2Deg;
             angle -= 90f; // to prevent it from pointing with the x axis
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            Quaternion targetRot = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * _rotationSpeed);
             // transform.up = directionToFace; // this works but doesn't tell you the angle
-            
+
             #endregion
-            
+
         }
     }
 }
