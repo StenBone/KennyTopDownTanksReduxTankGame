1089ace2eaf5ef78b6b528bc0afca8125e3124d9
Ben Stone <spector.8500@gmail.com>
2021-08-02 07:40:21 -0400

    Aiming GUI

--- Assets/Scripts/Tank/Aim.cs
+++ Assets/Scripts/Tank/Aim.cs
@@ -9,15 +9,18 @@ public class Aim : MonoBehaviour
 {
     // todo move this to a utility class, game manager thing?
     [SerializeField] 
-    private Camera _gameCamera;
-
+    private Camera gameCamera;
     [SerializeField] 
-    private float _rotationSpeed = 0.5f;
+    private Transform turretPivot;
+    [SerializeField] 
+    private Transform aimingGui;
+    [SerializeField] 
+    private float rotationSpeed = 0.5f;
     
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Assert(_gameCamera != null);
+        Debug.Assert(gameCamera != null);
     }
 
     // FixedUpdate since the barrel will have a collider and might hit a wall
@@ -28,27 +31,25 @@ public class Aim : MonoBehaviour
 
         if (mouse != null)
         {
-            var worldMousePos = _gameCamera.ScreenToWorldPoint(mouse.position.ReadValue());
+            var worldMousePos = gameCamera.ScreenToWorldPoint(mouse.position.ReadValue());
             //Debug.DrawLine(worldMousePos, worldMousePos + new Vector3(1, 0, 0), Color.green, 1f); // not showing up in Game View
             //Debug.DrawLine(worldMousePos, worldMousePos + new Vector3(0, 1, 0), Color.green);
 
             worldMousePos.z = transform.position.z; // ensure they are on the same plane
             Vector3 directionToFace = worldMousePos - transform.position;
-
-            // todo clean up angle calculation 
-            #region Angle Calculation needs to be cleaned up
-
+            
             float angle = Mathf.Atan2(directionToFace.y,directionToFace.x) * Mathf.Rad2Deg;
             angle -= 90f; // to prevent it from pointing with the x axis
             Quaternion targetRot = Quaternion.AngleAxis(angle, Vector3.forward);
-
-            transform.rotation = Quaternion.Lerp(transform.rotation, targetRot, Time.deltaTime * _rotationSpeed);
-            //transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * _rotationSpeed);
             
+            aimingGui.rotation = targetRot;
+            turretPivot.rotation = Quaternion.Lerp(turretPivot.rotation, targetRot, Time.deltaTime * rotationSpeed);
+            //transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * _rotationSpeed);
             // transform.up = directionToFace; // this works but doesn't tell you the angle
-
-            #endregion
-
+            
+            // calculate cross product
+            var crossProduct = Vector3.Cross(turretPivot.eulerAngles, targetRot.eulerAngles).normalized;
+            Debug.Log(crossProduct.ToString());
         }
     }
 }
